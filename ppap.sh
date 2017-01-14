5242cbc1bc08d3449faaaed184ff186593fa4a5b
seiyab <r108338@yahoo.co.jp>
2017-01-15 00:27:08 +0900

    upload ppap.sh

---
+++ ppap.sh
@@ -0,0 +1,8 @@
+say i have a pen
+say i have an apple
+say uh, apple pen
+say i have a pen
+say i have a pineapple
+say uh, pineapple pen
+say apple pen, pineapple pen
+say uh, pen pineapple apple pen
